da2e8e60d3cc91d31ffa5ace1681452d49c7a6eb
youpd <youpd@asiainfo.com>
2019-11-26 14:54:55 +0800

    A4

--- src/A.java
+++ src/A.java
@@ -2,6 +2,7 @@ public class A {
     private String age;
     private String name;
     private String test4="20191126A4";
+    private String test5="20191126A5";
     public String getName() {
         return name;
     }
