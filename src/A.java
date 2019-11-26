1753593ed6697faf5f851729ffbb151e6eebd793
youpd <youpd@asiainfo.com>
2019-11-26 14:34:40 +0800

    1

--- src/A.java
+++ src/A.java
@@ -1,22 +1,20 @@
 public class A {
+    private String age;
+    private String name;
 
-  private String age;
-  private String name;
-  private String test = "20191126";
+    public String getName() {
+        return name;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public String getAge() {
+        return age;
+    }
 
-  public String getAge() {
-    return age;
-  }
-
-  public void setAge(String age) {
-    this.age = age;
-  }
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
